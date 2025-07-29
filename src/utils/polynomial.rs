eb46291e4c0c041352593338c81a6ea03a1550e2
HiddenAndBound <ritwiksen9090@gmail.com>
2025-07-29 19:29:38 +0530

    adding mle utilities

---
+++ src/utils/polynomial.rs
@@ -0,0 +1,23 @@
+use core::slice;
+
+use p3_baby_bear::BabyBear;
+use p3_field::ExtensionField;
+
+pub struct MLE {
+    coeffs: Vec<BabyBear>,
+}
+
+impl MLE {
+    pub fn new(coeffs: Vec<BabyBear>) -> Self {
+        assert!(coeffs.len().is_power_of_two());
+        Self { coeffs }
+    }
+
+    pub fn len(&self) -> usize {
+        self.coeffs.len()
+    }
+    pub fn n_vars(&self) -> usize {
+        self.coeffs.len().trailing_zeros() as usize
+    }
+    pub fn evaluate(&self)-> ExtensionField
+}
