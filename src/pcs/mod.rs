ceaf15c24ce65eaa4e57eca7148f3e8b0093e6e0
HiddenAndBound <ritwiksen9090@gmail.com>
2025-08-16 22:47:36 +0530

    create encode function

--- src/pcs/mod.rs
+++ src/pcs/mod.rs
@@ -13,11 +13,7 @@ type Encoding = Vec<Fp>;
 
 const RATE: usize = 2;
 impl Basefold {
-    pub fn commit(poly: &MLE<Fp>, roots: Vec<Vec<Fp>>) -> (MerkleTree, Commitment, Encoding) {
-        assert!(
-            poly.len().is_power_of_two(),
-            "MLE's coefficients need to be a power of 2"
-        );
+    fn encode(poly: &MLE<Fp>, roots: &[Vec<Fp>]) -> Encoding {
         let mut buffer = vec![Fp::ZERO; poly.len() * RATE];
         buffer[0..poly.len()].copy_from_slice(poly.coeffs());
 
@@ -26,11 +22,25 @@ impl Basefold {
             "Root table not large enough to encode the MLE."
         );
 
-        BabyBear::forward_fft(&mut buffer, &roots);
+        BabyBear::forward_fft(&mut buffer, roots);
+        buffer
+    }
+
+    pub fn commit(poly: &MLE<Fp>, roots: Vec<Vec<Fp>>) -> (MerkleTree, Commitment, Encoding) {
+        assert!(
+            poly.len().is_power_of_two(),
+            "MLE's coefficients need to be a power of 2"
+        );
+        
+        let buffer = Self::encode(poly, &roots);
 
         let merkle_tree = MerkleTree::new(&buffer).unwrap();
         let commitment = merkle_tree.root();
 
         (merkle_tree, commitment, buffer)
+    }
+
+    pub fn evaluate(poly: &MLE<Fp>, eval_point: &[Fp], evaluation: Fp, merkle_tree:&MerkleTree, encoding: &Encoding)->{
+        
     }
 }
